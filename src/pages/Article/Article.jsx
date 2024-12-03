940c00d41d74848ae903a3d41925497a324b8915
Eric Liang <2435311105@qq.com>
2024-12-03 23:28:00 +0800

    add new react demo：note

--- src/pages/Article/Article.jsx
+++
@@ -1,8 +0,0 @@
-import { useSearchParams } from "react-router-dom"
-const Article = () => {
-  const [params] = useSearchParams()
-  console.log(params)
-  return <div>这里是id,{params.get("id")}</div>
-}
-
-export default Article
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,33 +1,9 @@
 import { createBrowserRouter } from "react-router-dom"
-import Article from "../pages/Article/Article"
-import Login from "../pages/Login/Login"
-import App from "../App"
-import Layout from "../pages/layout/Layout"
-import Main from "../pages/Main/Main"
-import Footer from "../pages/Footer/Footer"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "main",
-        element: <Main />,
-      },
-      {
-        path: "footer",
-        element: <Footer />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/article",
-    element: <Article />,
+    element: <div>Home</div>,
   },
 ])
 
